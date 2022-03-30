07ff2324b69ab1a9b1e922b1d338ab5a2eeaed3a
Akeem Palmer <akeemk.palmer@gmail.com>
2022-03-29 22:32:14 -0400

    Update: added UML Diagram PDF

--- src/main/java/com/example/comp1008assignment2/Player.java
+++ src/main/java/com/example/comp1008assignment2/Player.java
@@ -1,5 +1,6 @@
 package com.example.comp1008assignment2;
 
+//https://lucid.app/lucidchart/47dcb37e-3da7-42d6-99e9-4495f77be6c1/edit?beaconFlowId=D277759F349412D4&invitationId=inv_31f324cb-a29a-4b72-a9d6-cbb84053b3d1&page=HWEp-vi-RSFO#
 public class Player {
 
     private String name;
